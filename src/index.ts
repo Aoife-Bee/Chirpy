b2526b788ba3a4a887593fa9d79681d69cd224d9
Aoife Bee <aoifebeeof@gmail.com>
2026-02-17 15:23:56 -0500

    refactored validate using express middleware, renamed validate.ts to chirps.ts, added helper functions to json.ts

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,21 @@ import {
     } from "./api/middleware.js";
 import { handlerMetrics } from "./api/metrics.js";
 import { handlerReset } from "./api/reset.js";
-import { handlerValidateChirp } from "./api/validate.js";
+import { handlerValidateChirp } from "./api/chirps.js";
 const app = express();
 const PORT = 8080
 
 
 app.use(middlewareLogResponse);
+app.use(express.json());
 
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
-app.post("/api/validate_chirp", handlerValidateChirp)
+
 app.get("/api/healthz", handlerReadiness);
 app.get("/admin/metrics",handlerMetrics);
 app.post("/admin/reset", handlerReset);
 
-
+app.post("/api/validate_chirp", handlerValidateChirp)
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
