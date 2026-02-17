6d08282c228fa3746ccd8e2a4048a19010693600
Aoife Bee <aoifebeeof@gmail.com>
2026-02-17 17:22:00 -0500

    added drizzle configuration and initial user queries. configured drizzle orm with postgresql, implemented environment variable loading for secure db access. created user schema and a query to insert new users. added .env to gitignore.

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,20 @@
+process.loadEnvFile();
+
 type APIConfig = {
     fileServerHits: number;
+    dbURL: string;
 };
 
 export const config: APIConfig = {
     fileServerHits: 0,
+    dbURL: envOrThrow("DB_URL"),
+
 };
+
+function envOrThrow(key: string) {
+    const value = process.env[key];
+    if (!value) {
+        throw new Error(`Missing environment variable: ${key}`);
+    }
+    return value;
+}
