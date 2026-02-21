f145d7421631c7810d3e4c428a6a801536dd5ad0
Aoife Bee <aoifebeeof@gmail.com>
2026-02-21 17:29:19 -0500

    added refreshToken table to database and migrated database

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -21,8 +21,25 @@ export const chirps = pgTable("chirps", {
         .defaultNow()
         .$onUpdate(() => new Date()),
     body: varchar("body", { length: 256 }).notNull(),
-    userId: uuid("user_id").references(() => users.id, { onDelete: "cascade" }).notNull(),
+    userId: uuid("user_id")
+        .references(() => users.id, { onDelete: "cascade" }).notNull(),
 
 });
 
 export type NewChirp = typeof chirps.$inferInsert;
+
+export const refreshTokens = pgTable("refresh_tokens", {
+    token: varchar("token", { length: 256 }).primaryKey(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at")
+        .notNull()
+        .defaultNow()
+        .$onUpdate(() => new Date()),
+    userId: uuid("user_id")
+        .references(() => users.id, { onDelete: "cascade" }).notNull(),
+    expiresAt: timestamp("expires_at").notNull(),
+    revokedAt: timestamp("revoked_at"),
+
+});
+
+export type NewRefreshToken = typeof refreshTokens.$inferInsert;
